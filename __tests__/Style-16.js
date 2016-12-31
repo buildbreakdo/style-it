e963bd9fae7b1c1a2b71e3ec4e792d5261cebfee
Joshua Robinson <jbrobin2@gmail.com>
2016-12-31 15:58:15 +0530

    add test confirming root element with element and string children works

---
+++ __tests__/Style-16.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { findDOMNode, render } from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+
+const removeNewlines = (string) => (string.replace(/(\r\n|\n|\r)/gm, ''))
+
+import Style from '../src/index.js';
+
+describe('Style-02', () => {
+  it('does not produce an error when root element children are strings and components', () => {
+    const wrapper = TestUtils.renderIntoDocument(
+      <div>
+        <Style>
+          {`
+            .foo {
+              color: red;
+            }
+          `}
+
+          <div className="foo">
+            Time to talk
+            <div className="foo">
+              Football <span>yay</span>
+            </div>
+            <ul>
+              <li>test</li>
+            </ul>
+          </div>
+        </Style>
+      </div>
+    );
+
+    const rootNode = findDOMNode(wrapper).children[0];
+    const styleNode = rootNode.children[0];
+    const scopedClass = rootNode.className.split(' ').slice(-1)[0];
+
+    expect(rootNode.className).toEqual(`foo ${scopedClass}`);
+    expect(removeNewlines(styleNode.textContent)).toEqual(` .foo.${scopedClass} , .${scopedClass}  .foo { color: red; }`);
+  });
+});
