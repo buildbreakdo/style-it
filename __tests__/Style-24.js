d6c11721429ab9b93fc6a4365f7a37a53fdfda05
Joshua Robinson <jbrobin2@gmail.com>
2019-01-03 18:10:20 -0500

    Fixes #30 : Single declaration doesn't work without semicolon

---
+++ __tests__/Style-24.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { findDOMNode, render } from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+
+const removeNewlines = (string) => (string.replace(/(\r\n|\n|\r)/gm, ''))
+
+import Style from '../src/index.js';
+
+describe('Style-24', () => {
+  test('Correctly parses CSS declaration bodies with single property-value, no semi-colon, with new-line character value termination', () => {
+    const wrapper = TestUtils.renderIntoDocument(Style.it('.canvasPage {\n color: red\n}',
+      <div />
+    ));
+
+    const wrapperNode = findDOMNode(wrapper);
+    const wrapperStyleNode = wrapperNode.children[0];
+    const wrapperNodeScopedClass = wrapperNode.className.split(' ').slice(-1)[0];
+
+    expect(wrapperNode.className).toEqual(`${wrapperNodeScopedClass}`);
+    expect(removeNewlines(wrapperStyleNode.textContent)).toEqual(`.${wrapperNodeScopedClass} .canvasPage { color: red}`);
+  });
+});
